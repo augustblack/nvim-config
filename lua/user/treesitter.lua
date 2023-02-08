3cc33ad760110b72408ee408fc2c1c6ee25ca86b
August Black <augustblack@gmail.com>
2023-02-08 13:17:43 -0700

    fix minor errors

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,8 +3,8 @@ if not status_ok then
 	return
 end
 
-local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-if not status_ok then
+local s_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not s_ok then
 	return
 end
 
@@ -13,7 +13,7 @@ configs.setup({
   -- ensure_installed = "all", -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  
+
   highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
